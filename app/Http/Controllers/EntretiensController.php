0af568e62a65e5e9fde1e0ede84409a3fcee50e9
zineb19971231 <zinebakarras@gmail.com>
2026-05-24 03:19:36 +0100

    ajouter create view entretien , sa logique

--- app/Http/Controllers/EntretiensController.php
+++ app/Http/Controllers/EntretiensController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Entretien;
+use App\Models\Candidature;
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreEntretienRequest;
 
 class EntretiensController extends Controller
 {
@@ -18,18 +20,25 @@ class EntretiensController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
-        return view ('entretiens.create');
-    }
+   public function create()
+{
+    $candidatures = Candidature::where('user_id', auth()->id())->get();
+
+    return view('entretiens.create', compact('candidatures'));
+}
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
-        //
-    }
+    public function store(StoreEntretienRequest $request)
+{
+    $data = $request->validated();
+    
+    auth()->user()->entretiens()->create($data);
+
+    // 3. Redirection vers l'index avec un message de succès
+    return redirect()->route('entretiens.index')->with('success', 'Entretien créé avec succès.');
+}
 
     /**
      * Display the specified resource.
